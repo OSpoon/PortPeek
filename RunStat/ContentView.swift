661aaee70681663c7bc546363b7e2f88445dc7e7
OSpoon <zxin088@gmail.com>
2026-08-27 22:18:14 +0800

    Initial Commit

---
+++ RunStat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunStat
+//
+//  Created by OSpoon on 2026/8/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunStat/RunStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunStatApp.swift
+//  RunStat
+//
+//  Created by OSpoon on 2026/8/27.
+//
+
+import SwiftUI
+
+@main
+struct RunStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
